8093660d70c8e5c2b512c9abfc2f5ffd80012089
user <user@DESKTOP-GN0VGCQ>
2019-11-03 20:31:53 +0900

    초기 커밋.

---
+++ CSonDlg.cpp
@@ -0,0 +1,57 @@
+﻿// CSonDlg.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "Testing.h"
+#include "CSonDlg.h"
+#include "afxdialogex.h"
+
+
+// CSonDlg 대화 상자
+
+IMPLEMENT_DYNAMIC(CSonDlg, CDialogEx)
+
+CSonDlg::CSonDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DIALOG1, pParent)
+{
+
+}
+
+CSonDlg::~CSonDlg()
+{
+}
+
+void CSonDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CSonDlg, CDialogEx)
+	ON_BN_CLICKED(IDC_BUTTON1, &CSonDlg::OnBnClickedButton1)
+	ON_BN_CLICKED(IDC_BUTTON2, &CSonDlg::OnBnClickedButton2)
+END_MESSAGE_MAP()
+
+
+// CSonDlg 메시지 처리기
+
+#include "TestingDlg.h"
+
+void CSonDlg::OnBnClickedButton1()
+{
+	CTestingDlg* p = (CTestingDlg*)GetParent();
+	CString msg;
+	GetDlgItemText(IDC_EDIT1, msg);
+	if (!msg.IsEmpty()) {
+		p->m_List1.AddString(msg);
+	}
+
+}
+
+
+void CSonDlg::OnBnClickedButton2()
+{
+	CString cha;
+	GetDlgItemText(IDC_EDIT2, cha);
+	SetWindowText(cha);
+}
---
+++ TestingDlg.h
@@ -0,0 +1,37 @@
+﻿
+// TestingDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CTestingDlg 대화 상자
+class CTestingDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CTestingDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_TESTING_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	CListBox m_List1;
+	afx_msg void OnBnClickedButton1();
+};
